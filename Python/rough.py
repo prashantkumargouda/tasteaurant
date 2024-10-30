6efa6d869b5fdb2a79cae0b504fcee6015818a77
Rapth <prashantkumargouda17@gmail.com>
2024-10-31 00:26:58 +0530

    modified one file

--- Python/rough.py
+++ Python/rough.py
@@ -1,89 +1,85 @@
 class Node :
     def __init__(self,value) :
-        self.data = value 
+        self.data = value  
         self.next = None 
 
 class LinkedList :
-    def __init__(self,value ) :
+    def __init__(self,value) :
         newnode = Node(value) 
 
         self.head = newnode 
-        self.tail = newnode
-
+        self.tail = newnode 
         self.length = 1 
 
     def print_list(self) :
         temp = self.head 
 
-        while temp is not None :
+        while temp is not None : 
             print(temp.data) 
             temp = temp.next 
-
+    
     def append(self,value) :
         newnode = Node(value) 
 
-        if self.length == 0 :
-            self.head = newnode 
+        if self.head is None :
+            self.head = newnode
             self.tail = newnode 
 
         else :
             self.tail.next = newnode 
-            self.tail = newnode 
-
+            tail = newnode 
+        
         self.length += 1 
 
     def pop(self) :
         if self.length == 0 :
             return None 
-    
-        else :
-            temp = self.head 
-            pre = self.head 
 
-            while temp.next is not None :
-                pre = temp 
+        temp = self.head 
+        pre = None 
 
-                temp = temp.next 
-            
-            self.tail = pre
-            self.tail.next = None 
+        while temp.next is not None :
+            pre = temp 
+            temp = temp.next 
 
-        self.length -= 1 
+        self.tail = pre 
+        self.tail.next = None 
 
         if self.length == 0 :
             self.head = None 
             self.tail = None 
 
-        return temp.data 
-    
+        self.length -= 1 
+
     def prepend(self,value) :
         newnode = Node(value) 
 
-        if self.length == 0 :
+        if self.head is None :
             self.head = newnode 
             self.tail = newnode 
-        
         else :
-            temp = self.head 
-
-            newnode.next = self.head  
+            newnode.next = self.head 
             self.head = newnode 
 
         self.length += 1 
 
         return True 
- 
+    
     def pop_first(self) :
         if self.length == 0 :
             return None 
         
         else :
             temp = self.head 
-
+            
             self.head = self.head.next 
             temp.next = None 
 
-        self.length -= 1 
+            self.length -= 1 
 
-        if self.       
+            if self.length == 0 :
+                self.tail = None 
+            
 
+            return temp.data 
+        
