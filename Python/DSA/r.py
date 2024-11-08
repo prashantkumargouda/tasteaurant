fb72908e6a07a438c9bf57622f3aa50f286b0970
Rapth <prashantkumargouda17@gmail.com>
2024-11-08 18:05:37 +0530

    added one more file

--- Python/DSA/r.py
+++ Python/DSA/r.py
@@ -1,12 +1,1 @@
-class Node :
-    def __init__(self,value) :
-        self.value = value 
-        self.next = None
-
-class LinkedList :
-    def __init__(self,value) :
-        newnode = Node(value) 
-
-        self.head = newnode
-        self.tail = newnode 
 
