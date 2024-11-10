a731f0a73e99887284cdbd6dadb422933ed72c59
Rapth <prashantkumargouda17@gmail.com>
2024-11-10 22:10:20 +0530

    added more files

--- Python/DSA/r.cpp
+++ Python/DSA/r.cpp
@@ -1,3 +1,13 @@
+// trying to find the element with highest freq 
 
+#include <iostream> 
+using namespace std;
+
+int majority(int arr[], int n , int x , int y) {
+    for()
+}
+int main() {
+
+}
 
 
--- Python/DSA/r.py
+++ Python/DSA/r.py
@@ -1,1 +1,6 @@
+# finding the peak element and returning the index of the elements 
 
+def peak(arr) :
+    i = 1 
+
+    if( arr[i-1] )
