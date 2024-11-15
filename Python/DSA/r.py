edd30d3e9daeae49b4b49d025d9bc0358a92e01c
Rapth <prashantkumargouda17@gmail.com>
2024-11-16 00:09:37 +0530

    modified more files

--- Python/DSA/r.py
+++ Python/DSA/r.py
@@ -1,48 +1,17 @@
-# creating a linked list and modifying the linked list \
+# maximum element in a list 
+
+l = [5,3,1,6,5]  # output 5 
+
+max = 0 
 
 
-class Node:
-    def __init__(self,value) :
-        self.data = value
-        self.next = None 
+for i in range(5) :
+    if( l[i] > max ) :
+        max = l[i]  # 5 - - 6 - 
+    
 
-class LinkedList :
-    def __init__(self,value) :
-        newnode = Node(value) 
+print(max) 
 
-        self.head = newnode 
-        self.tail = newnode 
-        self.length = 1 
 
-    def print_list(self) :
-        temp = self.head 
 
-        while temp is not None :
-            print(temp.data) 
-            temp = temp.next 
-
-    def append(self,value) :
-        newnode = Node(value) 
-
-        if self.head is None :
-            self.head = newnode 
-            self.tail = newnode 
-
-        self.tail.next = newnode 
-        self.tail = newnode 
-
-        self.length += 1 
-
-    def pop(self,head) :
-        if self.head is None :
-            return None 
-        
-        temp = self.head 
-        pre = self.head 
-
-        while temp.next is not None :
-            pre = temp 
-            temp = temp.next 
-
-            
 
--- Python/DSA/tempCodeRunnerFile.py
+++ Python/DSA/tempCodeRunnerFile.py
@@ -1,1 +1,1 @@
-print(count)
+i = i+1 
