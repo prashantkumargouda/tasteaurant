5dfe469f01a9144bab133aed35ffd7ebcbae3986
Rapth <prashantkumargouda17@gmail.com>
2024-12-20 22:04:06 +0530

    added

---
+++ c++/recursion/fibo.cpp
@@ -0,0 +1,21 @@
+// fibonacci series 
+
+#include <iostream>
+using namespace std;
+
+int fibo( int n ) {
+    if( n == 1 ){
+        return 0 ;
+    }
+    if( n == 2 ) {
+        return 1 ;
+    }
+    else {
+        return fibo(n-1) + fibo(n-2) ;
+    }
+}
+int main(){
+    cout << fibo(8) << endl; 
+
+    return 0 ;
+}
