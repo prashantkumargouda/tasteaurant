3e661e3b791a72512e2279299e13cdf8d064578c
Rapth <prashantkumargouda17@gmail.com>
2024-12-22 13:29:25 +0530

    Added more files

--- c++/recursion/fibo.cpp
+++ c++/recursion/fibo.cpp
@@ -3,20 +3,23 @@
 #include <iostream>
 using namespace std;
 
-int fibo( int n ) {
-    if( n == 1 ){
-        return 0 ;
+int fibo(int n) {
+    if (n <= 0) {
+        return -1; // Return -1 for invalid input
     }
-    if( n == 2 ) {
-        return 1 ;
+    if (n == 1) {
+        return 0;
     }
-    else {
-        return fibo(n-1) + fibo(n-2) ;
+    if (n == 2) {
+        return 1;
     }
+    return fibo(n - 1) + fibo(n - 2);
 }
-int main(){
-    cout << fibo(8) << endl; 
 
-    return 0 ;
+int main() {
+    int n = 8;
+    cout << "Fibonacci number at position " << n << " is " << fibo(n) << endl;
+
+    return 0;
 }
 
